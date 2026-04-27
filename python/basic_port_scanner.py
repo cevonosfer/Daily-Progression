90e16f94c624fee0e4b032034eb8ac49b8801d40
cevonosfer <cevheryuksel05@gmail.com>
2026-04-27 16:11:56 +0200

    #Added a sloppy CVE lookup function to search the NVD lib using nvdlib

--- python/basic_port_scanner.py
+++ python/basic_port_scanner.py
@@ -1,6 +1,7 @@
 import argparse
 import socket 
 from threading import Lock,Thread
+import nvdlib
 
 parser = argparse.ArgumentParser(description = "basic tool")
 parser.add_argument("target" , help="ip or hostname")
@@ -15,11 +16,25 @@ def iterator(host, port):
             s.settimeout(3)
             s.connect((host,port))
             s.sendall(b"HEAD / HTTP/1.0\r\n\r\n")
-            banner = s.recv(1024)    
-            print(f"open : {port} -- {banner[:80][:22]}")
+            banner = s.recv(1024).decode()  
+            print(f"open : {port} -- {banner}")
+            cve_lookup(banner)
     except (socket.timeout , ConnectionRefusedError , ConnectionResetError , OSError):
         print(f"closed : {port}")
 
+def cve_lookup(banner):
+    keyword = banner.replace("/", " ").replace("-"," ").replace("_", " ").split("\r\n")[0][:50]
+    
+    try:
+        r = nvdlib.searchCVE(keywordSearch=keyword , limit=3)
+        for eachCVE in r:
+            print(eachCVE.id, str(eachCVE.score[0]), eachCVE.url, eachCVE.cpe)
+        if not r: 
+            print (f"no CVE found for {keyword}")
+
+    except Exception as e:    
+        print(f"failed {e}")
+        
 threads = []
 
 for port in args.ports:
