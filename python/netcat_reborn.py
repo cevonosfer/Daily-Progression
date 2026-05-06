12aa924af6e8bfbe49bc6541788ca43249771fba
cevonosfer <cevheryuksel05@gmail.com>
2026-05-06 18:15:41 +0200

    #Continuing to build the basic nc tool

--- python/netcat_reborn.py
+++ python/netcat_reborn.py
@@ -1,40 +1,86 @@
 import argparse
 import socket
+import os
 
 parser = argparse.ArgumentParser(description="netcat")
-host = parser.add_argument("host", help="ip or hostname")
+target = parser.add_argument("-t", help="ip or hostname")
 ports = parser.add_argument("-p", "--ports", nargs="+", type=int)
-parser.add_argument("-l", help="initiates a listener(TCP)")
-parser.add_argument("-lu", help="initiates a listener(UDP)")
+files = parser.add_argument("-f", "--files", nargs="+", type=str)
+messages = parser.add_argument("-m", "--message", nargs="+", type=str)
+parser.add_argument("-l", action="store_true", help="initiates a listener(TCP)")
 parser.add_argument("-zv", help="port scanner")
 args = parser.parse_args()
 
 
 
-def scanner(host,ports):
+def scanner(target,ports):
     try:
         with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            s.connect((host,ports))
-            s.send()
+            s.connect((target,ports))
+            s.send(b"HEAD / HTTP/1.0\r\n\r\n")
             banner = s.recv(1024).decode()
             print(f"open : {ports} -- {banner}")
     except(socket.timeout, ConnectionRefusedError, ConnectionResetError):
         print(f"closed{ports}")
 
-def listener(host,ports):
+def listener(target,ports):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        bind_ip = host
-        bind_port = ports
-        s.bind((bind_ip, bind_port))
+        s.bind((target, ports))
         s.listen(10)
+        print(f"Listening on : {target} : {ports}")
 
-def listenerUDP(host,ports):
-    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
-        bind_ip = host
-        bind_port = ports
-        s.bind((bind_ip, bind_port))
-        s.listen(10)
+    while True:     
+        client, addr = s.accept()
+        request = client.recv(1024)
+        decoded = request.decode()
+        if decoded.startswith("FILE"):
+            __, file_name, file_size, _ = decoded.split("\n", 3)
+            file_size = int(file_size)
+            print(file_name, file_size)
 
-def sender(): #client for sending messages and files#
+            file = open(file_name , "wb")
+            done = False
+            is_finished = b""
+
+            while not done:
+                data = client.recv(1024)
+                print(f"received chunk: {data}")  # see exactly what's arriving
+                is_finished += data
+                if is_finished.endswith(b"<FINISH>"):
+                    file.write(is_finished)
+                    done = True 
+            print(f"file received from : {addr[0]} : {addr[1]}")
+            client.close()
+            file.close()
+
+        print(f"Connection from : {addr[0]} : {addr[1]}")
+        print(f"Recieved: {decoded}") 
+        client.send("Ping received".encode()) 
+        client.close()
+
+def sender(target,ports,files,messages):
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as c:
+        c.connect((target,ports))
+
+        if files:
+            location = os.path.abspath(files)
+            files = open(location, "rb")
+            size = os.path.getsize(files)
+            name = files
+
+            header = f"{name}\n{size}\n"
+            c.send(header.encode())
+
+            data = files.read()
+            c.sendall(data)
+            c.send(b"<FINISH>")
+            files.close()
+            c.close()
+
+        message = messages
+        c.send(message.encode())
+        response = c.recv(1024)
+        print(response.decode())
+
 
 def main(): #main for the argument conditions and functions#
