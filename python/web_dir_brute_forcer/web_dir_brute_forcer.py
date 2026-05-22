fb0a8fb868490f327062260feb92b12016f43265
cevonosfer <cevheryuksel05@gmail.com>
2026-05-22 18:56:31 +0300

    #Added recursiveness as well as command line options. Next will be to save the results to a file

--- python/web_dir_brute_forcer/web_dir_brute_forcer.py
+++ python/web_dir_brute_forcer/web_dir_brute_forcer.py
@@ -1,55 +1,72 @@
-import requests
-from concurrent.futures import ThreadPoolExecutor
 import asyncio
 import aiohttp
 import time 
+import argparse
 
+parser = argparse.ArgumentParser(description="recursive web dir brute forcer")
+parser.add_argument("-u", "--url", help="base url")
+parser.add_argument("-w", "--wordlist", help="wordlist file")
+parser.add_argument("-d", "--depth", help="recursiveness depth", type=int, default=3)
+args = parser.parse_args()
 
 BASE_URL = "http://scanme.nmap.org"
 WORDLIST_FILE = "python\web_dir_brute_forcer\wordlist.txt"
 
 found_paths = []
+visited = set()
 
-def load_words():
+def load_paths():
     with open(WORDLIST_FILE, "r") as f:
-        words = []
+        paths = []
 
         for line in f:
             line = line.strip()
             if line:
-                words.append(line)
-        return words
+                paths.append(line)
+        return paths
 
-async def check_path(session: aiohttp.ClientSession, path, sem):
-    url = f"{BASE_URL}/{path}"
+async def check_path(session: aiohttp.ClientSession, url, sem):
     async with sem:
         try:
             async with session.get(url, timeout=aiohttp.ClientTimeout(total=5)) as r:
-
                 if r.status in [200, 301, 302, 403]: #useful codes 200:ok , 301,302:redirect , 403:exists but denied
-                    print(f"{r.status} :: {url}")
                     found_paths.append((url, r.status))
+                    return url
+                return None
+        except (aiohttp.ClientError,asyncio.TimeoutError):
+            return None
 
-        except requests.RequestException:
-            print(f"error :: {url}")
+async def scan(session, base_url, paths, sem, depth):
+    if depth == 0 or base_url in visited:
+        return
+    visited.add(base_url)
+    tasks = [check_path(session, f"{base_url}/{p}", sem) for p in paths]
+    results = await asyncio.gather(*tasks)
+    
+    # results is a list like [None, "/admin", None, "/login", None...]
+    # filter out the Nones
+    found = [r for r in results if r is not None]
+    
+    # recurse into each found path
+    for url in found:
+        await scan(session, url, paths, sem, depth - 1)
+
 
 async def main():
     start = time.perf_counter()
-    words = load_words()
+    paths = load_paths()
 
     print(f"starting scan on {BASE_URL}")
-    print(f"loaded {len(words)} paths\n")
+    print(f"loaded {len(paths)} paths\n")
 
-    sem = asyncio.Semaphore(100)
+    sem = asyncio.Semaphore(150)
 
     async with aiohttp.ClientSession() as session:
-        tasks = [check_path(session, word, sem) for word in words]
-        await asyncio.gather(*tasks)
+        await scan(session, BASE_URL, paths, sem, args.depth)
 
     print(f"Found {len(found_paths)} interesting paths")
     for url, code in found_paths:
-        if code != 404:
-            print(f"{code} :: {url}")
+        print(f"{code} :: {url}")
     print(f"\nScan completed in {time.perf_counter() - start:.2f}s")
 
 if __name__ == "__main__":
